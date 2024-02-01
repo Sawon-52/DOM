fbf114d5d5b6abcb868db1344870d4f93893c044
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-02-01 21:11:27 +0600

    add some text and button

---
+++ script.js
@@ -0,0 +1,19 @@
+// const box1 = document.querySelector('#box1');
+
+// console.log(box1.innerHTML);
+
+// const box1 = document.querySelector('.boxes');
+
+// console.dir(box1);
+
+// console.dir(box1);
+// var arrayBox1 = Array.prototype.slice.call(box1);
+// console.log(arrayBox1);
+
+
+// const list = document.querySelector('#list')
+
+// console.dir(list.childern);
+
+const univers = document.querySelector('*');
+console.dir(univers);
