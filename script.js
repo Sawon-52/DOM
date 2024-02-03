84be1be78a7449fccc6a896b682d910967b02eb2
Sawon-52 <mehedihasansawon.52@gmail.com>
2024-02-03 10:42:44 +0600

    create add button and add new box in a box div

--- script.js
+++ script.js
@@ -18,16 +18,41 @@
 // const univers = document.querySelector('*');
 // console.dir(univers);
 
-const header = document.querySelector('.header');
-// console.dir(header.nextElementSibling);
+// const header = document.querySelector('.header');
+// // console.dir(header.nextElementSibling);
 
 
-let div = document.createElement('div');
-div.id = 'note';
-div.className = 'content';
-div.innerHTML = '<p> Hi My name is mehedi hasan </p>';
-document.body.appendChild(div);
+// let div = document.createElement('div');
+// div.id = 'note';
+// div.className = 'content';
+// div.innerHTML = '<p> Hi My name is mehedi hasan </p>';
+// document.body.appendChild(div);
 
-const body = document.querySelector('body');
-console.dir(body.children);
+// const body = document.querySelector('body');
+// console.dir(body.children);
+
+
+
+
+const box = document.querySelector('.boxes');
+
+// var box5 = document.createElement('div');
+// box5.className = 'box';
+// box5.id = 'box5';
+// box5.innerHTML = '<h3> This is box Five </h3>';
+// box.appendChild(box5);
+
+const button = document.querySelector('.button');
+console.dir(button);
+
+button.addEventListener('click',  function(){
+    var box5 = document.createElement('div');
+    box5.className = 'box';
+    box5.id = 'box5';
+    box5.innerHTML = '<h3> This is box Five </h3>';
+    box.appendChild(box5);
+
+    
+})
+
 
